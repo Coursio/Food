d3f8d91b8f823decbaae552908b0d280b957f756
Kim Gunnarsson <kim@morgondag.nu>
2013-06-25 08:28:26 +0200

    Added JS Flavor

---
+++ food.js
@@ -0,0 +1,16 @@
+var food = [
+	'Cuba',
+	'Sue-Ellen',
+	'Thai',
+	'Moon',
+	'Burgers',
+	'Eko',
+	'Starve',
+	'Ica'
+]
+
+var rand = food[Math.floor(Math.random() * food.length)];
+
+console.log('\n \nFOOD PICKER 2000\n \n','You got: ',rand , '\n Now Go!\n \n')
+
+// call with node food
